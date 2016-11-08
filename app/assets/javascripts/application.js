8c5704afd0d67f1e7a50e1b05d07ce4d71f61413
Faizan Ahmad <faizan.ahmad@engintechnologies.com>
2016-11-08 13:21:55 +0500

    layout fixes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,21 +1,24 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
+// This is a manifest file that'll be compiled into application, which will include all the files
 // listed below.
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
 // or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
 //
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
 //
-// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery.min
+//= require jquery
+//= require jquery_ujs
 //= require jquery-ui
-//= require plugin/pace/pace.min
-//= require app.config
-//= require bootstrap/bootstrap.min
-//= require plugin/jquery-validate/jquery.validate.min
-//= require plugin/masked-input/jquery.maskedinput.min
-//= require app.min
 
+// IMPORTANT: APP CONFIG
+//= require app.config.seed
+
+// BOOTSTRAP JS
+//= require bootstrap/bootstrap
+
+// MAIN APP JS FILE
+//= require app.seed
