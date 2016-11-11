79847fc79e86594e38cc5af8adbb488c56fa708a
ahsanulhaq <ahsan.ulhaq@engintechnologies.com>
2016-11-11 11:44:34 +0500

    Landing Page Setting

---
+++ app/assets/javascripts/landing.js
@@ -0,0 +1,12 @@
+
+//= reqiure jquery
+//= require jquery-ui
+//= require bootstrap/bootstrap.min
+//= require plugins/classie
+//= require plugins/gnmenu
+//= require plugins/jquery.scrollUp
+//= require plugins/nivo-lightbox.min
+//= require plugins/smoothscroll
+//= require plugins/jquery.themepunch.plugins.min
+//= require plugins/jquery.themepunch.revolution.min
+//= require custom
--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,6 +2,8 @@ class StaticController < ApplicationController
 
   skip_before_action :authenticate_user!
 
+  layout 'landing'
+
   def index
   end
 end
