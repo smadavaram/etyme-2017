09a1866677a3ddb485f91c32a0b7b9549c1b30bb
Faizan Ahmad <faizan.ahmad@engintechnologies.com>
2016-12-07 18:42:55 +0500

    tinymc fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -219,16 +219,16 @@ module ApplicationHelper
                     title: 'Jobs',
                     content: "<span class='menu-item-parent'> Jobs </span>"
                 },
-                {
-                    href: candidate_job_applications_path,
-                    title: 'Job Applications',
-                    content: "<span class='menu-item-parent'> Job Applications </span>"
-                },
-                {
-                    href: candidate_contracts_path,
-                    title: 'Contracts',
-                    content: "<span class='menu-item-parent'> Contracts </span>"
-                },
+                # {
+                #     href: candidate_job_applications_path,
+                #     title: 'Job Applications',
+                #     content: "<span class='menu-item-parent'> Job Applications </span>"
+                # },
+                # {
+                #     href: candidate_contracts_path,
+                #     title: 'Contracts',
+                #     content: "<span class='menu-item-parent'> Contracts </span>"
+                # },
             ]
         }
     ]
