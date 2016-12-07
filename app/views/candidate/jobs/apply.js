29158c2f87a793612dff42585c37732b29937171
Faizan Ahmad <faizan.ahmad@engintechnologies.com>
2016-12-07 19:01:29 +0500

    tiny mc jquery fix

--- app/views/candidate/jobs/apply.js
+++ app/views/candidate/jobs/apply.js
@@ -1,11 +1,4 @@
 $('#job_invitation').html("<%= j render('candidate/job_applications/partials/job_application_modal' , job: @job , job_application: @job_application , accept_state: true) %>");
-tinymce.init({
-    selector: '.tinymces',
-    menubar: false ,
-    plugins: 'image table link paste contextmenu textpattern autolink',
-    insert_toolbar: 'quicktable',
-    selection_toolbar: 'bold italic | quicklink h2 h3 blockquote'
-});
 $('#job-invite-<%=@job.id %>').modal('show');
 //tinymce.init({
 //    selector: '.r#{@job.id}',
