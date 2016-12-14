bc45ae5ce7d4dce613799ecd97e5354b97dcf82a
Faizan Ahmad <faizan.ahmad@engintechnologies.com>
2016-12-14 15:40:08 +0500

    typo fixes

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -32,6 +32,11 @@ class Contract < ActiveRecord::Base
   scope :accepted , -> {where(status: 1)}
   scope :rejected , -> {where(status: 2)}
 
+  #Validates
+  validates           :start_date,  presence:   true
+  validates           :end_date,    presence:   true
+
+
   # Nested Attributes
   accepts_nested_attributes_for :contract_terms, allow_destroy: true ,reject_if: :all_blank
 
