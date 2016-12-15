a51452501de571dd29d553033159da91bbe08417
Faizan Ahmad <faizan.ahmad@engintechnologies.com>
2016-12-15 19:04:27 +0500

    time_frequency fixes

--- config/initializers/constant.rb
+++ config/initializers/constant.rb
@@ -1,3 +1,3 @@
 SITE_NAME = "Etyme"
 COMPANY_TYPES = [["HiringManager", 0], ["Vendor", 1]]
-TIMESHEET_FREQUENCY = { 0 => 7, 1 => 3 , 3 => 30 , 4 => 3 }
+TIMESHEET_FREQUENCY = { 0 => 7, 1 => 3 , 3 => 30 , 4 => 15 }
