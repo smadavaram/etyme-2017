5de8f22b3d70a1abbadf78c9021469c4704fd45c
Faizan Ahmad <faizan.ahmad@engintechnologies.com>
2016-12-15 19:22:39 +0500

    typo fixes

--- config/initializers/constant.rb
+++ config/initializers/constant.rb
@@ -1,3 +1,3 @@
 SITE_NAME = "Etyme"
 COMPANY_TYPES = [["HiringManager", 0], ["Vendor", 1]]
-TIMESHEET_FREQUENCY = { 0 => 7, 1 => 3 , 3 => 30 , 4 => 15 }
+TIMESHEET_FREQUENCY = { 0 => 7, 1 => 3 , 2 => 30 , 3 => 15 }
