dc679fad7e49f1f0a465c0325b5cf706aef492cf
Faizan Ahmad <faizan.ahmad@engintechnologies.com>
2016-12-20 21:18:54 +0500

    typo changes

--- app/controllers/company/attachments_controller.rb
+++ app/controllers/company/attachments_controller.rb
@@ -4,7 +4,7 @@ class Company::AttachmentsController < Company::BaseController
   # add_breadcrumb current_company.name.titleize, "#", :title => ""
 
   def index
-    @attachments_page=Attachment.paginate(:page => params[:page], :per_page => 20)
+    @attachments_page=current_company.attachments.paginate(:page => params[:page], :per_page => 20)
     add_breadcrumb "Company Documents", attachments_path(current_company), :title => ""
   end
 
@@ -14,7 +14,7 @@ class Company::AttachmentsController < Company::BaseController
 
   private
   def set_attachments
-    all_attachemts=Attachment.all
+    all_attachemts=current_company.attachments
     @attachments=all_attachemts.group_by(&:attachable_type)
   end
 end
