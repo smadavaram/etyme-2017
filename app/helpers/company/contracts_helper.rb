eeb5bda5a31a778fb49f18f370bb3fdaa03babb9
Umair Azhar <muhammadumairazhar9@gmail.com>
2017-01-04 14:13:25 +0500

    Ui Fixes

--- app/helpers/company/contracts_helper.rb
+++ app/helpers/company/contracts_helper.rb
@@ -1,2 +1,17 @@
 module Company::ContractsHelper
+  def show_recipient_name contractable
+    if contractable.class.name == "Candidate"
+      contractable.full_name
+    else
+      contractable.name.titleize
+    end
+  end
+
+  def show_recipient_picture contractable
+    if contractable.class.name == "Candidate"
+      contractable.photo
+    else
+      contractable.logo
+    end
+  end
 end
