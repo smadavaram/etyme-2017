ac759876f8891900df1200999a83529df308f329
ahsanulhaq <ahsan.ulhaq@engintechnologies.com>
2017-01-09 16:12:33 +0500

    Candidate seperated

--- app/models/user.rb
+++ app/models/user.rb
@@ -54,7 +54,7 @@ class User < ActiveRecord::Base
   # validates_presence_of :email
   # validates_uniqueness_of :email
 
-  validates_numericality_of :phone
+  # validates_numericality_of :phone
 
   after_create :create_address
 
