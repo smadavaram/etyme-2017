3b6310ba28445af7a1e06be70922ef4ce6778082
Umair Azhar <muhammadumairazhar9@gmail.com>
2017-01-18 15:28:17 +0500

    issue resolved

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -13,7 +13,16 @@ WickedPdf.config = {
   # one of the wkhtmltopdf-binary family of gems.
   # exe_path: '/usr/local/bin/wkhtmltopdf'
   #   or
-  exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
+  exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf'),
+  :page_size  => "A4",
+  :dpi => '300',
+  :orientation=> "Portrait",
+  :no_background    => true,
+  :margin           => {:top    => 0, # default 10 (mm)
+                        :bottom => 0,
+                        :left   => 0,
+                        :right  => 0},
+
 
   # Layout file to be used for all PDFs
   # (but can be overridden in `render :pdf` calls)
