971728925b40675a947c704c586249a33f3ac831
ahsanulhaq <ahsan.ulhaq@engintechnologies.com>
2017-01-20 20:15:43 +0500

    Home Page

--- app/assets/javascripts/landing.js
+++ app/assets/javascripts/landing.js
@@ -1,14 +1,14 @@
-
-//= reqiure jquery
+//= require jquery
+//= require jquery_ujs
 //= require jquery-ui
-//= require landing/bootstrap/bootstrap.min
-//= require landing/plugins/classie
-//= require landing/plugins/gnmenu
-//= require landing/plugins/jquery.scrollUp
-//= require landing/plugins/nivo-lightbox.min
-//= require landing/plugins/smoothscroll
-//= require landing/plugins/jquery.themepunch.plugins.min
-//= require landing/plugins/jquery.themepunch.revolution.min
-//= require landing/custom
+//= require bootstrap/bootstrap.min
+//= require home/js/jquery.flot
+//= require home/js/dropzone
+//= require home/js/fancybox/jquery.fancybox
+//= require home/js/fancybox/helpers/jquery.fancybox-buttons
+//= require home/js/fancybox/helpers/jquery.fancybox-media
+//= require home/js/global
 //= require global
+
+
 
