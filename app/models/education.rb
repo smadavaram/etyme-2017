6731e441521dc77f469f0d0bef761b3e6c46c0d0
Umair Azhar <muhammadumairazhar9@gmail.com>
2017-02-06 12:47:57 +0500

    fix the exception issue and and confirm box before delete

--- app/models/education.rb
+++ app/models/education.rb
@@ -8,12 +8,13 @@ class Education < ActiveRecord::Base
   private
 
   def completion_year_is_greater_than_start_year
-    if self.completion_year < self.start_year
-      errors.add(:completion_year," should be greater than start year")
-      return false
-    else
-      return true
+    if self.completion_year.present?
+      if self.completion_year < self.start_year
+        errors.add(:completion_year," should be greater than start year")
+        return false
+      else
+        return true
+      end
     end
-
   end
 end
