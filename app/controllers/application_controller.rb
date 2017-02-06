efdb1ae2bc4a95d6f0c85b8107fe2453b69cdad9
Umair Azhar <muhammadumairazhar9@gmail.com>
2017-02-06 16:46:42 +0500

    fix authenticity issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,7 @@ class ApplicationController < ActionController::Base
   rescue_from AbstractController::ActionNotFound, with: :render_not_found if Rails.env.production?
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found if Rails.env.production?
   rescue_from StandardError,with: :render_not_found if Rails.env.production?
+  rescue_from Exception::NoMethodError,with: :render_not_found if Rails.env.production?
 
 
 
