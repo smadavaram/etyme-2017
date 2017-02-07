c6875febe133457a4d1dead893e22b5b273526f9
ahsanulhaq <ahsan.ulhaq@engintechnologies.com>
2017-02-07 19:25:06 +0500

    Custom Fileds Required option on Job Create and Job Application

--- app/models/custom_field.rb
+++ app/models/custom_field.rb
@@ -1,5 +1,7 @@
 class CustomField < ActiveRecord::Base
 
   belongs_to :customizable, polymorphic: true
+  validates  :value , presence: true,on: :create  , if: Proc.new{ |cf| cf.required && cf.customizable_type=="JobApplication"}
+  # validates  required:presence: :true,on: :update ,if
 
 end
