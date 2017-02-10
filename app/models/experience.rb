b8d9b019b4f0098cd5fbf01f74a42ecc2938655a
Umair Azhar <muhammadumairazhar9@gmail.com>
2017-02-10 11:27:57 +0500

    live server fixes

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -10,13 +10,13 @@ class Experience < ActiveRecord::Base
   private
 
     def end_date_is_greater_than_start_date
-      if self.end_date < self.start_date
-        errors.add(:end_date," should be greater than start date")
-        return false
-      else
-        return true
+      if self.end_date.present?
+        if self.end_date < self.start_date
+          errors.add(:end_date," should be greater than start date")
+          return false
+        else
+          return true
+        end
       end
-
     end
-
 end
