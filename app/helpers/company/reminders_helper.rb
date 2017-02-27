902f987a13790ac43889fc0027015b5537153abd
ahsanulhaq <ahsan.ulhaq@engintechnologies.com>
2017-02-27 17:42:46 +0500

    Reminder And other Fixes

--- app/helpers/company/reminders_helper.rb
+++ app/helpers/company/reminders_helper.rb
@@ -1,2 +1,5 @@
 module Company::RemindersHelper
+  def find_reminderable_last_reminder(reminderable)
+    current_user.try(:reminders).where(reminderable_type: reminderable.try(:class) ,reminderable_id: reminderable.id).try(:last)
+  end
 end
--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -4,13 +4,13 @@ class Reminder < ActiveRecord::Base
 
   validates :title            ,presence: :true
   validates :remind_at        ,presence: :true
+  validates :remind_at, date: { after_or_equal_to: Time.now, message: ' can not be  in Past .'}
 
   belongs_to     :user
   belongs_to     :reminderable ,polymorphic:  :true
 
   after_create   :add_reminder_in_delayed_job
 
-  private
 
   def add_reminder_in_delayed_job
     self.delay(run_at: self.remind_at ).send_reminder_email
@@ -19,4 +19,5 @@ class Reminder < ActiveRecord::Base
   def send_reminder_email
     UserMailer.send_remind_email(self)
   end
+
 end
