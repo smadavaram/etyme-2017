2c486ee9333d71b8a31f3877a8d5f0d45aad74fe
ahsan <ahsan.ulhaq@engintechnologies.com>
2017-04-12 20:07:19 +0500

    UI Fixes

--- app/assets/javascripts/static.js
+++ app/assets/javascripts/static.js
@@ -3,6 +3,7 @@
 //= require static/js/jQuery-2.1.4.min
 //= require jquery_ujs
 //= require jquery-ui
+
 //= require static/js/bootstrap.min
 //= require static/js/waypoints.min
 //= require static/js/jquery.animateNumbers
@@ -10,6 +11,8 @@
 //= require static/js/kafe.flexslider
 //= require static/js/jquery.appear
 //= require static/js/jquery.easypiechart
+//= require static/js/vegas/vegas
+//= require static/js/vegas/jquery.appear
 //= require static/js/kafe
 //= require jquery.amaran
 //= require plugin/select2/select2.min
