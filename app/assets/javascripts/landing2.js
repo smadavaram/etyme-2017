86bc938a701c1e76afc1c23c2e832065c6df9656
ahsan <ahsan.ulhaq@engintechnologies.com>
2017-04-13 14:49:30 +0500

    UI Tweaks

--- app/assets/javascripts/landing2.js
+++
@@ -1,8 +0,0 @@
-//= require libs/jquery-2.1.1.min.js
-//= require bootstrap/bootstrap.js
-//= require landing2/js/SmoothScroll.js
-//= require landing/plugins/nivo-lightbox.min.js
-//= require landing2/js/jquery.isotope.js
-//= require landing2/js/jqBootstrapValidation.js
-//= require landing2/js/contact_me.js
-//= require landing2/js/main.js
