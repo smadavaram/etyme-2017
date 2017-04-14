d80b9ebe0ebb6c55147364b3c39c5b5c93d5ab6f
ahsan <ahsan.ulhaq@engintechnologies.com>
2017-04-14 17:16:21 +0500

    Flash Cancel Functionality

--- app/helpers/static_helper.rb
+++ app/helpers/static_helper.rb
@@ -12,12 +12,19 @@ module StaticHelper
   end
   def flash_class(level)
     case level
-      when :notice then "alert alert-info"
-      when :success then "alert alert-success"
-      when :error then "alert alert-error"
-      when :alert then "alert alert-error"
-      when :errors then "alert alert-error"
+      when :notice then 'alert alert-info alert-dismissible'
+      when :success then 'alert alert-success alert-dismissible'
+      when :error then 'alert alert-error alert-dismissible'
+      when :alert then 'alert alert-error alert-dismissible'
+      when :errors then 'alert alert-error alert-dismissible'
     end
+  end
+
+  def flash_message(key,value)
+    data = "<div role='alert' class= '#{flash_class(key.to_sym)}'><button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+    <span aria-hidden='true'>&times;</span>
+  </button> #{value.is_a?(String) ? value : value.first} </div>"
+    data.html_safe
   end
 
 end
