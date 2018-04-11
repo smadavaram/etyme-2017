9f39bf9c51dd12410973dfa9c8964688c24f648d
Chirag_Mangroliya <kishan.sutex.kp@gmail.com>
2018-04-11 18:14:32 +0530

    Set chante with attachemnt

--- app/controllers/conversation_messages_controller.rb
+++ app/controllers/conversation_messages_controller.rb
@@ -10,57 +10,65 @@ class ConversationMessagesController < ApplicationController
         @conversation.chatable.groupables.each do |gm|
           GroupMsgNotify.create(group_id: @conversation.chatable.id, member: gm.groupable, conversation_message: message)
           ActionCable.server.broadcast "Message_#{gm.groupable_type}_#{gm.groupable_id}",
-                                       message_id: message.id,
-                                       message: message.body,
-                                       user_type: message.userable.class.to_s,
-                                       user: message.userable.id,
-                                       recipient_type: gm.groupable_type,
-                                       recipient_id: gm.groupable_id,
-                                       unread_message_count: 0, # Conversation.joins(:conversation_messages).where("(senderable_type = ? AND senderable_id = ? ) OR (recipientable_type = ? AND recipientable_id = ?)", recipient.class.to_s, recipient.id, recipient.class.to_s, recipient.id).where.not(conversation_messages: {is_read: true, userable: recipient}).uniq.count,
-                                       conversation_id: @conversation.id,
-                                       chat_type: "Group",
-                                       group_id: @conversation.chatable.id,
+                                       msg_id: message.id,
+                                       msg: message.body,
+                                       msg_att: message.attachment_file,
+                                       msg_att_name: message.file_name,
+                                       usr_typ: message.userable.class.to_s,
+                                       usr: message.userable.id,
+                                       recpt_type: gm.groupable_type,
+                                       recpt_id: gm.groupable_id,
+                                       unread_msg_cnt: 0, # Conversation.joins(:conversation_messages).where("(senderable_type = ? AND senderable_id = ? ) OR (recipientable_type = ? AND recipientable_id = ?)", recipient.class.to_s, recipient.id, recipient.class.to_s, recipient.id).where.not(conversation_messages: {is_read: true, userable: recipient}).uniq.count,
+                                       con_id: @conversation.id,
+                                       chat_typ: "Group",
+                                       grp_id: @conversation.chatable.id,
                                        dom: "#conversation_#{@conversation.id}"
         end
         @conversation.chatable.company.users.each do |usr|
           GroupMsgNotify.create(group_id: @conversation.chatable.id, member: usr, conversation_message: message)
           ActionCable.server.broadcast "Message_#{usr.class}_#{usr.id}",
-                                       message_id: message.id,
-                                       message: message.body,
-                                       user_type: message.userable.class.to_s,
-                                       user: message.userable.id,
-                                       recipient_type: usr.class,
-                                       recipient_id: usr.id,
-                                       unread_message_count: 0, # Conversation.joins(:conversation_messages).where("(senderable_type = ? AND senderable_id = ? ) OR (recipientable_type = ? AND recipientable_id = ?)", recipient.class.to_s, recipient.id, recipient.class.to_s, recipient.id).where.not(conversation_messages: {is_read: true, userable: recipient}).uniq.count,
-                                       conversation_id: @conversation.id,
-                                       chat_type: "Group",
-                                       group_id: @conversation.chatable.id,
+                                       msg_id: message.id,
+                                       msg: message.body,
+                                       msg_att: message.attachment_file,
+                                       msg_att_name: message.file_name,
+                                       usr_typ: message.userable.class.to_s,
+                                       usr: message.userable.id,
+                                       recpt_type: usr.class,
+                                       recpt_id: usr.id,
+                                       unread_msg_cnt: 0, # Conversation.joins(:conversation_messages).where("(senderable_type = ? AND senderable_id = ? ) OR (recipientable_type = ? AND recipientable_id = ?)", recipient.class.to_s, recipient.id, recipient.class.to_s, recipient.id).where.not(conversation_messages: {is_read: true, userable: recipient}).uniq.count,
+                                       con_id: @conversation.id,
+                                       chat_typ: "Group",
+                                       grp_id: @conversation.chatable.id,
                                        dom: "#conversation_#{@conversation.id}"
         end
       else
         recipient = (@conversation.recipientable == get_current_user ? @conversation.senderable : @conversation.recipientable )
         ActionCable.server.broadcast "Message_#{recipient.class.to_s}_#{recipient.id}",
-                                     message_id: message.id,
-                                     message: message.body,
-                                     user_type: message.userable.class.to_s,
-                                     user: message.userable.id,
-                                     recipient_type: recipient.class.to_s,
-                                     recipient_id: recipient.id,
-                                     unread_message_count: Conversation.joins(:conversation_messages).where("(senderable_type = ? AND senderable_id = ? ) OR (recipientable_type = ? AND recipientable_id = ?)", recipient.class.to_s, recipient.id, recipient.class.to_s, recipient.id).where.not(conversation_messages: {is_read: true, userable: recipient}).uniq.count,
-                                     conversation_id: @conversation.id,
-                                     chat_type: "OneToOne",
+                                     msg_id: message.id,
+                                     msg: message.body,
+                                     msg_att: message.attachment_file,
+                                     msg_att_name: message.file_name,
+                                     usr_typ: message.userable.class.to_s,
+                                     usr: message.userable.id,
+                                     recpt_type: recipient.class.to_s,
+                                     recpt_id: recipient.id,
+                                     unread_msg_cnt: 0, #Conversation.joins(:conversation_messages).where("(senderable_type = ? AND senderable_id = ? ) OR (recipientable_type = ? AND recipientable_id = ?)", recipient.class.to_s, recipient.id, recipient.class.to_s, recipient.id).where.not(conversation_messages: {is_read: true, userable: recipient}).uniq.count,
+                                     con_id: @conversation.id,
+                                     chat_typ: "OneToOne",
                                      dom: "#conversation_#{@conversation.id}"
 
         ActionCable.server.broadcast "Message_#{get_current_user.class.to_s}_#{get_current_user.id}",
-                                     message_id: message.id,
-                                     message: message.body,
-                                     user_type: message.userable.class.to_s,
-                                     user: message.userable.id,
-                                     recipient_type: recipient.class.to_s,
-                                     recipient_id: recipient.id,
-                                     unread_message_count: Conversation.joins(:conversation_messages).where("(senderable_type = ? AND senderable_id = ? ) OR (recipientable_type = ? AND recipientable_id = ?)", recipient.class.to_s, recipient.id, recipient.class.to_s, recipient.id).where.not(conversation_messages: {is_read: true, userable: recipient}).uniq.count,
-                                     conversation_id: @conversation.id,
-                                     chat_type: "OneToOne",
+                                     msg_id: message.id,
+                                     msg: message.body,
+                                     msg_att: message.attachment_file,
+                                     msg_att_name: message.file_name,
+                                     usr_typ: message.userable.class.to_s,
+                                     usr: message.userable.id,
+                                     recpt_type: recipient.class.to_s,
+                                     recpt_id: recipient.id,
+                                     unread_msg_cnt: 0, # Conversation.joins(:conversation_messages).where("(senderable_type = ? AND senderable_id = ? ) OR (recipientable_type = ? AND recipientable_id = ?)", recipient.class.to_s, recipient.id, recipient.class.to_s, recipient.id).where.not(conversation_messages: {is_read: true, userable: recipient}).uniq.count,
+                                     con_id: @conversation.id,
+                                     chat_typ: "OneToOne",
                                      dom: "#conversation_#{@conversation.id}"
       end
     end
@@ -84,7 +92,7 @@ class ConversationMessagesController < ApplicationController
   end
 
   def message_params
-    params.require(:conversation_message).permit(:body)
+    params.require(:conversation_message).permit(:body, :attachment_file, :file_name, :file_size, :file_type)
   end
 
   def get_current_user
---
+++ db/migrate/20180410054928_add_attachemnt_fields_in_chat.rb
@@ -0,0 +1,8 @@
+class AddAttachemntFieldsInChat < ActiveRecord::Migration[5.1]
+  def change
+    add_column :conversation_messages, :attachment_file, :string
+    add_column :conversation_messages, :file_name, :string
+    add_column :conversation_messages, :file_size, :string
+    add_column :conversation_messages, :file_type, :string
+  end
+end
