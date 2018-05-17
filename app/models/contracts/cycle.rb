068b6ef02518cdcb4875ad33e0d7abd2e37cf688
Chirag_Mangroliya <kishan.sutex.kp@gmail.com>
2018-05-17 18:11:53 +0530

    Set next date fopr timesheet

---
+++ app/models/contracts/cycle.rb
@@ -0,0 +1,138 @@
+module Contracts
+  class Cycle < Struct.new(:contract)
+    delegate :contract_cycles, :timesheets, :invoices, :sell_contracts, :buy_contracts, :contract_salary_histories, :to => :contract
+
+    def set_timesheet_submit
+      if contract_cycle.present?
+        next_date =  get_next_date(ts_time_sheet_frequency, ts_date_1, ts_date_2, ts_end_of_month, ts_day_of_week, contract_cycle.cycle_date )
+        start_date = contract_cycle.end_date + 1.day
+      else
+        next_date =  get_next_date(ts_time_sheet_frequency, ts_date_1, ts_date_2, ts_end_of_month, ts_day_of_week, contract.start_date-1.day)
+        start_date = contract.start_date
+      end
+
+      ta_next_date =  get_next_date(ta_time_sheet_frequency, ta_date_1, ta_date_2, ta_end_of_month, ta_day_of_week, Time.now-1.day)
+      while next_date <= Time.now
+        next_next_date =  get_next_date(ts_time_sheet_frequency, ts_date_1, ts_date_2, ts_end_of_month, ts_day_of_week, next_date)
+        cycle = add_cycle("Timesheet submit", next_date, start_date, next_date, "TimesheetSubmit", buy_contract.candidate_id, next_next_date, "TimesheetApprove", ta_next_date)
+        add_timesheet(start_date, next_date, buy_contract.candidate.full_name, buy_contract.candidate_id, cycle.id)
+
+        next_date = next_next_date
+        start_date = cycle.end_date + 1.day
+      end
+    end
+
+    def set_timesheet_approve
+    end
+
+    private
+
+    def add_cycle(note, cycle_date, start_date, end_date, cycle_type, candidate_id, next_date, next_action, next_action_date)
+      contract_cycles.create(note: note,
+                             cycle_date: cycle_date,
+                             start_date: start_date,
+                             end_date: end_date,
+                             cycle_type: cycle_type,
+                             candidate_id: candidate_id,
+                             next_date: next_date,
+                             next_action: next_action,
+                             next_action_date: next_action_date
+      )
+    end
+
+    def add_timesheet(start_date, next_date, candidate_name, candidate_id, cycle_id)
+      Timesheet.create(
+          contract_id: contract_id,
+          start_date: start_date,
+          end_date: next_date,
+          candidate_name: candidate_name,
+          candidate_id: candidate_id,
+          ts_cycle_id: cycle_id
+      )
+    end
+
+    def contract_id
+      contract.id
+    end
+
+    def contract_cycle
+      contract_cycles.where(cycle_type: "TimesheetSubmit").order("created_at DESC").first
+    end
+
+    def buy_contract
+      buy_contracts.first
+    end
+
+    def ts_time_sheet_frequency
+      buy_contract.time_sheet
+    end
+
+    def ts_date_1
+      buy_contract.ts_date_1
+    end
+
+    def ts_date_2
+      buy_contract.ts_date_2
+    end
+
+    def ts_end_of_month
+      buy_contract.ts_end_of_month
+    end
+
+    def ts_day_of_week
+      buy_contract.ts_day_of_week
+    end
+
+    def ta_time_sheet_frequency
+      buy_contract.ts_approve
+    end
+
+    def ta_date_1
+      buy_contract.ta_date_1
+    end
+
+    def ta_date_2
+      buy_contract.ta_date_2
+    end
+
+    def ta_end_of_month
+      buy_contract.ta_end_of_month
+    end
+
+    def ta_day_of_week
+      buy_contract.ta_day_of_week
+    end
+
+    def get_next_date(time_sheet_frequency, date_1, date_2, end_of_month, day_of_week, last_date = Date.today - 1.day )
+      if time_sheet_frequency == "daily" || time_sheet_frequency == "immediately"
+        time_sheet_date = last_date + 1.days
+      elsif time_sheet_frequency == "weekly"
+        time_sheet_date = date_of_next(day_of_week, last_date)
+      elsif time_sheet_frequency == "biweekly"
+        time_sheet_date = date_of_next(day_of_week, (date_of_next(day_of_week, last_date)))
+      elsif  time_sheet_frequency == "twice a month"
+        time_sheet_date = date_1.day == last_date.day ? (end_of_month ? last_date.end_of_month : (last_date.change(day: date_2.day))) : ((last_date + 1.month).change(day: date_1.day))
+      elsif  time_sheet_frequency == "monthly"
+        if end_of_month
+          time_sheet_date = last_date.end_of_month
+        else
+          time_sheet_date = last_date.change(day: date_1.day)
+        end
+      end
+      return time_sheet_date
+    end
+
+    def date_of_next(day, current_date)
+      s = current_date.wday
+      n = Date.parse(day).wday
+      if s < n
+        i =  n - s
+      elsif s > n
+        i =  n + ( 7 - s )
+      else
+        i =  7
+      end
+      current_date + i.days
+    end
+  end
+end
