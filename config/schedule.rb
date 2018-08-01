8ed9c3bab9906e9210591082f04564986f68b14b
Bhaumik <bhaumikgithub@gmail.com>
2018-08-01 11:45:11 +0530

    resolved bugs

--- config/schedule.rb
+++ config/schedule.rb
@@ -29,7 +29,7 @@ every 1.day, :at => '11:59 pm' do
 end
 
 every 1.day, :at => '12:01 am' do
-  runner "Contract.cycle"
+  runner "Contract.set_cycle"
 end
 
 # Learn more: http://github.com/javan/whenever
