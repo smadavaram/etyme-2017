16762aff6284d459778cc67e7643d646019e4031
tanay <tanays.tps@gmail.com>
2019-02-11 09:41:54 +0530

    fix comments

--- app/models/change_rate.rb
+++ app/models/change_rate.rb
@@ -1,8 +1,5 @@
 class ChangeRate < ApplicationRecord
   belongs_to :contract
-  scope :get_date_range,		->(contract_id, type){ where(contract_id: contract_id, rate_type: type).pluck(:from_date, :to_date, :rate) }
-
-  def self.rate(date, contract_id, type)
-    rate = get_date_range(contract_id,type).map{|x| x[2] if date.between?(x[0], x[1])}.compact.first
-  end
+  scope :get_date_range, ->(contract_id, type){ where(contract_id: contract_id, rate_type: type).pluck(:from_date, :to_date, :rate) }
+  scope :date_range, ->(date, contract_id, type) { get_date_range(contract_id,type).map{|x| x[2] if date.between?(x[0], x[1])}.compact.first }
 end
