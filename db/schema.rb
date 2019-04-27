92e1493f69eb176663af58286ae22396c1d5131e
kishan <kishan.sutex.kp@gmail.com>
2019-04-27 16:18:36 +0530

    user porfile update.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190422220355) do
+ActiveRecord::Schema.define(version: 20190427055659) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -1628,6 +1628,46 @@ ActiveRecord::Schema.define(version: 20190422220355) do
     t.datetime "updated_at", null: false
     t.string "file"
     t.index ["timesheet_log_id"], name: "index_transactions_on_timesheet_log_id"
+  end
+
+  create_table "user_certificates", force: :cascade do |t|
+    t.bigint "user_id"
+    t.date "end_date"
+    t.date "start_date"
+    t.string "institute"
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_user_certificates_on_user_id"
+  end
+
+  create_table "user_educations", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "degree_level"
+    t.string "degree_title"
+    t.string "cgpa_grade"
+    t.date "completion_year"
+    t.date "start_year"
+    t.string "institute"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_user_educations_on_user_id"
+  end
+
+  create_table "user_work_clients", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "name"
+    t.string "industry"
+    t.date "end_date"
+    t.date "start_date"
+    t.string "reference_name"
+    t.string "reference_phone"
+    t.string "reference_email"
+    t.text "project_description"
+    t.text "role"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_user_work_clients_on_user_id"
   end
 
   create_table "users", id: :serial, force: :cascade do |t|
