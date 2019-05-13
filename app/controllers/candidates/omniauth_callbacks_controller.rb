80b98d7ced68757f3ca6a93f567cdeba9ca6ad26
Muhammd Rizwan <rizu3661@gmail.com>
2019-05-13 17:39:20 +0500

    add linkedin user name and photo

--- app/controllers/candidates/omniauth_callbacks_controller.rb
+++ app/controllers/candidates/omniauth_callbacks_controller.rb
@@ -59,9 +59,8 @@ class Candidates::OmniauthCallbacksController < Devise::OmniauthCallbacksControl
         token_expires: request.env["omniauth.auth"].credentials.expired_at,
         first_name: request.env["omniauth.auth"].info.first_name,
         last_name: request.env["omniauth.auth"].info.last_name,
-        profile_image: request.env["omniauth.auth"].info.image
+        profile_image: request.env["omniauth.auth"].info.picture_url
     }
-
     if current_user && current_user.present?
       session[:token]=@auth[:token]
       redirect_to set_redirection(current_user)
