9d96c364d60631680c10d686e627e44b98f8c0af
Umair Raza <umair.raza101@gmail.com>
2019-05-15 19:26:51 +0500

    notification for adding recruiter

---
+++ app/controllers/candidate/designations_controller.rb
@@ -0,0 +1,17 @@
+class Candidate::DesignationsController < Candidate::BaseController
+  skip_before_action :authenticate_candidate!, only: :accept
+  before_action :set_designation, only: :accept
+
+  def accept
+    if @designation.verified!
+      flash[:success] = "Invitation Is Accepted"
+    else
+      flash[:error] = "Something went wrong"
+    end
+    redirect_back(fallback_location: root_path)
+  end
+
+  def set_designation
+    @designation = Designation.find_by(id: params[:id])
+  end
+end
