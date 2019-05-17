bf7085e3334616a04ad744aa3fd1212acc2b3a90
khuram <khuramdogar73@gmail.com>
2019-05-17 10:51:47 +0500

    change the links and send conversation id

--- app/helpers/company/job_applications_helper.rb
+++ app/helpers/company/job_applications_helper.rb
@@ -3,10 +3,10 @@ module Company::JobApplicationsHelper
     # status: [ :pending_review ,:rejected , :short_listed,:interviewing,:hired ]
     if  app.job.company == current_company && has_permission?('manage_job_applications')
       btn = ""
-      btn += link_to 'Short list' ,short_list_job_application_path(app) , method: :post   ,  class: "btn #{btn_size_class}  btn-primary mt-12 margin-right-5 #{!app.pending_review? ? 'disabled' : "" }"
-      btn += link_to 'Reject' , reject_job_application_path(app), method: :post  , class: "btn #{btn_size_class}  btn-primary mt-12 margin-right-5 #{app.hired? || app.contract.present? || app.rejected? ? 'disabled' : "" }"
-      btn += link_to 'Interview' , interview_job_application_path(app) , method: :post  , class: "btn #{btn_size_class}  btn-primary mt-12 margin-right-5 #{!app.short_listed? ? 'disabled' : "" }"
-      btn += link_to 'Hire' , hire_job_application_path(app) , method: :post  ,  class:  " btn #{btn_size_class}  btn-primary mt-12 margin-right-5 #{!app.interviewing? ? 'disabled' : "" }"
+      btn += link_to 'Short list' ,short_list_job_application_path(app, conversation_id: @conversation.id) , method: :post   ,  class: "btn #{btn_size_class}  btn-primary mt-12 margin-right-5 #{!app.pending_review? ? 'disabled' : "" }"
+      btn += link_to 'Reject' , reject_job_application_path(app, conversation_id: @conversation.id), method: :post  , class: "btn #{btn_size_class}  btn-primary mt-12 margin-right-5 #{app.hired? || app.contract.present? || app.rejected? ? 'disabled' : "" }"
+      btn += link_to 'Interview' , interview_job_application_path(app, conversation_id: @conversation.id) , method: :post  , class: "btn #{btn_size_class}  btn-primary mt-12 margin-right-5 #{!app.short_listed? ? 'disabled' : "" }"
+      btn += link_to 'Hire' , hire_job_application_path(app, conversation_id: @conversation.id) , method: :post  ,  class:  " btn #{btn_size_class}  btn-primary mt-12 margin-right-5 #{!app.interviewing? ? 'disabled' : "" }"
       if app.is_candidate_applicant? &&  !current_company.consultants.where(candidate_id: app.applicationable_id).present?
         btn += link_to 'Start On-Boarding' , new_job_application_consultant_path(app)  ,   class: "contract btn #{btn_size_class} btn-default mt-12 margin-right-5 #{  !app.hired? || app.contract.present? ? 'disabled' : "" }"
       else
