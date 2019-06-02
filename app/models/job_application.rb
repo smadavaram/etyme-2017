5635b45c89207d687c93ac41a30024b4fd6e389c
Umair Raza <umair.raza101@gmail.com>
2019-06-02 14:03:15 +0500

    ordered application statuses

--- app/models/job_application.rb
+++ app/models/job_application.rb
@@ -5,8 +5,8 @@ class JobApplication < ApplicationRecord
   has_paper_trail only: [:rate_per_hour]
 
 
-  enum status: [ :applied, :prescreen, :pending_review ,:rejected , :short_listed,:interviewing,:hired, :rate_confirmation,:client_submission ]
-  enum application_type: [:direct , :candidate_direct , :vendor_direct , :invitation, :witout_registration,:with_recurator]
+  enum status: [:applied, :short_listed, :prescreen, :rate_confirmation, :client_submission, :interviewing, :hired, :rejected, :pending_review]
+  enum application_type: [:direct, :candidate_direct, :vendor_direct, :invitation, :witout_registration, :with_recurator]
 
   belongs_to :job_invitation, optional: true
   belongs_to :applicationable, polymorphic: true, optional: true
