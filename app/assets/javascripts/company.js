c61fbb0b20a5187afeab2c4a4dda5caac38c8312
Umair Raza <umair.raza101@gmail.com>
2019-06-12 16:24:42 +0500

    navigation completed.

--- app/assets/javascripts/company.js
+++ app/assets/javascripts/company.js
@@ -206,7 +206,19 @@ $(document).ready(function () {
       }
     });
   }
-
+  var collapse = document.getElementsByClassName("sidebar_collapsible");
+  var i;
+  for (i = 0; i < collapse.length; i++) {
+    collapse[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    });
+  }
 
 
 });
