bf983550338886f3579482e1c60c11fd3f4969a7
Umair Raza <umair.raza101@gmail.com>
2019-06-14 00:17:30 +0500

    candidate navigation stubs are added

--- app/assets/javascripts/candidate.js
+++ app/assets/javascripts/candidate.js
@@ -181,3 +181,16 @@ $("input[name='candidate[designation_status]']").click(function () {
     $('#employee-designations').addClass('display-none');
   }
 });
+var collapse = document.getElementsByClassName("sidebar_collapsible");
+var i;
+for (i = 0; i < collapse.length; i++) {
+  collapse[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
