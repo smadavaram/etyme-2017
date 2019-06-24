5168857c700ba9a4ea28d396de35479e54770279
Umair Raza <umair.raza101@gmail.com>
2019-06-24 20:25:10 +0500

    added the docusign support structure

---
+++ db/migrate/20190624122117_create_docusign.rb
@@ -0,0 +1,13 @@
+class CreateDocusign < ActiveRecord::Migration[5.1]
+  def change
+    create_table :docusigns do |t|
+      t.string :ds_expires_at
+      t.string :ds_user_name
+      t.string :ds_access_token
+      t.string :ds_account_id
+      t.string :ds_account_name
+      t.string :ds_base_path
+      t.integer :company_id
+    end
+  end
+end
