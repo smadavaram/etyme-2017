5cddcd4d8957e9681cd229637dc109ad1ec8c093
Muhammad Umair raza <umair.raza101@gmail.com>
2019-07-24 16:29:18 +0500

    candidate contracts modifications display

--- app/controllers/candidate/contracts_controller.rb
+++ app/controllers/candidate/contracts_controller.rb
@@ -1,7 +1,20 @@
 class Candidate::ContractsController < Candidate::BaseController
-
+  before_action :set_contract, only: [:show]
+  add_breadcrumb "Home",'/candidate'
   def index
-    @contracts = Contract.joins(:buy_contracts).where(buy_contracts: {candidate_id: current_candidate.id})
+    add_breadcrumb "HR-Contract", candidate_contracts_path
+    @contracts = Contract.where(candidate: current_candidate)
+  end
+
+  def show
+    add_breadcrumb "HR-Contract", candidate_contracts_path
+    add_breadcrumb @contract.project_name, candidate_contract_path(@contract)
+  end
+
+
+  private
+  def set_contract
+    @contract = Contract.find_by_id(params[:id])
   end
 
 end
