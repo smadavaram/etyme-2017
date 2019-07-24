07f12783e3b8bd92e2e36ea27dc076eaed96bd78
Muhammad Umair raza <umair.raza101@gmail.com>
2019-07-24 18:26:05 +0500

    added show page of candidate contracts

--- app/controllers/candidate/contracts_controller.rb
+++ app/controllers/candidate/contracts_controller.rb
@@ -3,18 +3,23 @@ class Candidate::ContractsController < Candidate::BaseController
   add_breadcrumb "Home",'/candidate'
   def index
     add_breadcrumb "HR-Contract", candidate_contracts_path
-    @contracts = Contract.where(candidate: current_candidate)
+    @contracts = Contract.where.not(status: :draft).where(candidate: current_candidate)
   end
 
   def show
-    add_breadcrumb "HR-Contract", candidate_contracts_path
-    add_breadcrumb @contract.project_name, candidate_contract_path(@contract)
+    if @contract
+      add_breadcrumb "HR-Contract", candidate_contracts_path
+      add_breadcrumb @contract.project_name, candidate_contract_path(@contract)
+    else
+      flash[:errors] = ['Contract Does not exists or destroyed']
+      redirect_to candidate_contracts_path
+    end
   end
 
 
   private
   def set_contract
-    @contract = Contract.find_by_id(params[:id])
+    @contract = Contract.where.not(status: :draft).find_by_id(params[:id])
   end
 
 end
