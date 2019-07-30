6ecf61c68c6d64af99589077df62e4cf9ebf8bef
Muhammad Umair Raza <umair.raza101@gmail.com>
2019-07-30 18:12:30 +0500

    change contracts to contract

--- app/models/timesheet.rb
+++ app/models/timesheet.rb
@@ -225,7 +225,7 @@ class Timesheet < ApplicationRecord
   def self.date_of_next(day_of_week,con_cycle)
     # binding.pry
     day_of_week = DateTime.parse(day_of_week).wday
-    ts_day_of_week = DateTime.parse(con_cycle&.contract&.buy_contracts.ts_day_of_week).wday if con_cycle.contract.buy_contract.time_sheet == 'weekly'
+    ts_day_of_week = DateTime.parse(con_cycle&.contract&.buy_contract.ts_day_of_week).wday if con_cycle.contract.buy_contract.time_sheet == 'weekly'
     date = con_cycle.start_date.to_date + ((day_of_week - con_cycle.start_date.to_date.wday) % 7)
     if day_of_week >= con_cycle.start_date.wday
       date = (date - con_cycle.start_date.to_date <= 5) && con_cycle.start_date.wday != 0 ? date+7.days : date
