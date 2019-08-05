660abdb8c1e9b61fd86322f4b192b84c3ea388ed
Muhammad Umair Raza <umair.raza101@gmail.com>
2019-08-05 23:15:31 +0500

    buy side approvel completed

--- app/models/approval.rb
+++ app/models/approval.rb
@@ -1,5 +1,5 @@
 class Approval < ApplicationRecord
-  enum approvable_type: [:timesheet, :invoice, :expense, :timesheet_approve]
+  enum approvable_type: [:timesheet, :invoice, :expense, :timesheet_approve, :slaray_calculation, :slaray_process]
   belongs_to :user
   belongs_to :contractable, polymorphic: true
 end
