84256c02255e8a565cb133e56bc9d7154bf622a6
Muhammad Umair raza <umair.raza101@gmail.com>
2019-08-28 19:30:12 +0500

    sovren WIP

--- app/models/candidates_resume.rb
+++ app/models/candidates_resume.rb
@@ -1,14 +1,14 @@
 class CandidatesResume < ApplicationRecord
   belongs_to :candidate, optional: true
   after_save :build_profile
-  scope :primary_resume, -> { where(is_primary: true).first}
+  scope :primary_resume, -> { where(is_primary: true).first }
 
   private
 
   def build_profile
     begin
       if self.candidate.first_resume?
-        response = ResumeParser.new(resume).binary_parse
+        response = ResumeParser.new(resume).sovren_parse
         self.candidate.build_profile(JSON.parse(response.body)) if response.code == "200"
       end
     rescue => e
--- app/services/resume_parser.rb
+++ app/services/resume_parser.rb
@@ -24,10 +24,10 @@ class ResumeParser
         'Content-Type' => 'application/json',
         'Accept' => "application/json"
     }
-    http = Net::HTTP.new(uri.host,uri.port)
+    http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    response = http.post(uri.path,body.to_json,headers)
+    response = http.post(uri.path, body.to_json, headers)
   end
 
   def parse
@@ -42,10 +42,63 @@ class ResumeParser
         'Content-Type' => 'application/json',
         'Accept' => "application/json"
     }
-    http = Net::HTTP.new(uri.host,uri.port)
+    http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    response = http.post(uri.path,body.to_json,headers)
+    response = http.post(uri.path, body.to_json, headers)
+  end
+
+  def sovren_parse
+    account_id = ENV['sovren_client_id']
+    service_key = ENV['sovren_service_key']
+    uri = URI.parse("https://rest.resumeparsing.com/v9/parser/resume")
+    request = Net::HTTP::Post.new(uri)
+    request.content_type = "application/json"
+    request["Accept"] = "application/json"
+    request["Sovren-Accountid"] = account_id
+    request["Sovren-Servicekey"] = service_key
+    request.body = {
+        "DocumentAsBase64String": Base64.encode64(open(@resume_url).read),
+        "GeocodeOptions": {
+            "IncludeGeocoding": true,
+            "Provider": "string",
+            "ProviderKey": "string",
+            "PostalAddress": {
+                "CountryCode": "string",
+                "PostalCode": "string",
+                "Region": "string",
+                "Municipality": "string",
+                "AddressLine": "string"
+            },
+            "GeoCoordinates": {
+                "Latitude": 0,
+                "Longitude": 0
+            }
+        },
+        "IndexingOptions": {
+            "IndexId": "string",
+            "DocumentId": "string",
+            "CustomIds": [
+                "string"
+            ]
+        },
+        "OutputHtml": true,
+        "OutputRtf": true,
+        "OutputCandidateImage": true,
+        "OutputPdf": true,
+        "Configuration": "string",
+        "RevisionDate": "string",
+        "SkillsData": [
+            "string"
+        ],
+        "NormalizerData": "string"
+    }.to_json
+    req_options = {
+        use_ssl: uri.scheme == " https ",
+    }
+    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+      http.request(request)
+    end
   end
 
 end
