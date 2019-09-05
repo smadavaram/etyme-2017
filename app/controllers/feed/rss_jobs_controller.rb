be3bf09b0092c437d2ab81d7eafda076ca663e2c
Muhammad Umair raza <umair.raza101@gmail.com>
2019-09-05 16:37:51 +0500

    fetch all blogs

--- app/controllers/feed/rss_jobs_controller.rb
+++ app/controllers/feed/rss_jobs_controller.rb
@@ -28,7 +28,7 @@ class Feed::RssJobsController < ApplicationController
     end
  end
   def blog_feed
-    @jobs = Job.active.is_public.where(:listing_type=>"Blog").where(:status =>"Published")
+    @jobs = Job.where(:listing_type=>"Blog").where(:status =>"Published")
     if params[:company_id].present?
       @jobs = @jobs.where(company_id: params[:company_id])
       if params[:job_id].present?
