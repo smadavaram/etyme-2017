0e36d7c5a19598ac3eb0a06b4949af326f114f69
Muhammad Umair Raza <umair.raza101@gmail.com>
2019-10-03 15:16:33 +0500

    make change in owen jobs index

--- app/models/contract_sell_business_detail.rb
+++ app/models/contract_sell_business_detail.rb
@@ -4,10 +4,10 @@ class ContractSellBusinessDetail < ApplicationRecord
 	belongs_to :company_contact, optional: true
 	before_save :enforce_admin
 
-
-
-  
     def enforce_admin
       self.role = :admin  if self.sell_contract.count_contract_bussiness_details == 0 
     end
+
+
+  
 end
