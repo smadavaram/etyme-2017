08b37526fe175fe220db66c4d7a925e8fa3a3d19
Muhammad Umair Raza <umair.raza101@gmail.com>
2019-11-05 12:42:12 +0500

    remove dublicate helper and use delegate

--- app/datatables/application_datatable.rb
+++ app/datatables/application_datatable.rb
@@ -17,58 +17,22 @@ class ApplicationDatatable < AjaxDatatablesRails::ActiveRecord
   def_delegator :@view, :contact_widget
   def_delegator :@view, :colorfull_text
   def_delegator :@view, :chat_link
+  def_delegator :@view, :get_initial
+  def_delegator :@view, :bind_initials
+  def_delegator :@view, :colorfull_text
+  def_delegator :@view, :do_ellipsis
+  def_delegator :@view, :default_user_img
+  def_delegator :@view, :user_image
+  def_delegator :@view, :image_alt
+  def_delegator :@view, :entity_image
+
 
   def initialize(params, opts = {})
     @view = opts[:view_context]
     super
   end
 
-  def do_ellipsis(value, length = 20)
-    if value
-      post_fix = value.length > length ? '...' : ''
-      content_tag(:span, "#{value[0..length].strip}#{post_fix}", class: 'ellipsis', title: value).html_safe
-    end
-  end
-
   def company_logo_and_name company
     image_tag(company.logo, class: "data-table-image").html_safe + " " + company.name
-  end
-  def get_initial(name_text)
-    name_text.first.capitalize
-  end
-
-  def bind_initials(first_name,last_name)
-    content_tag(:span, get_initial(first_name)+"."+get_initial(last_name), title: first_name+" "+last_name).html_safe
-  end
-
-  def colorfull_text(value,color_code)
-    content_tag(:span, value, style: "color: #{color_code}")
-  end
-  def default_user_img(first_name,last_name,circle_div_class='circle')
-      content_tag(:span, bind_initials(first_name,last_name),class: "#{circle_div_class}")
-  end
-  def user_image(user,attrs)
-    return  if user.nil?
-    if user.photo.present?
-      image_tag(user.photo, style: "#{attrs[:style]}", class: "data-table-image #{attrs[:class]}" ,title: "#{attrs[:title]}",alt: image_alt(user) ).html_safe
-    else
-      entity_image(user.first_name,user.last_name)
-    end
-  end
-  def image_alt(user = nil)
-    begin
-      "#{get_initial(user.first_name)}.#{get_initial(user.last_name)}"
-    rescue
-      'N/A'
-    end
-  end
-  def entity_image(first_name,last_name,circle_div_class='circle',default_img_classes='')
-    default_img =''
-    if first_name=='' || last_name=='' 
-      default_img = default_img+ "<img src='#{ActionController::Base.helpers.asset_path('avatars/m_sunny_big.png')}' alt= '#{first_name} #{last_name}' class='#{default_img_classes}'/>"
-    else
-      default_img = default_img + default_user_img(first_name,last_name,circle_div_class)
-    end
-    return default_img.html_safe
   end
 end
