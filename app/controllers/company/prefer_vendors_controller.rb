10000c28d5f2a3da56b86db3de43e7077fcae4d2
Muhammad Umair Raza <umair.raza101@gmail.com>
2019-11-26 17:44:07 +0500

    refactor search skill of candidate and jobs  query to one query

--- app/controllers/company/prefer_vendors_controller.rb
+++ app/controllers/company/prefer_vendors_controller.rb
@@ -20,7 +20,6 @@ class Company::PreferVendorsController < Company::BaseController
         title: params[:Job_titles],
         department: params[:job_departments],
         industry: params[:job_industry],
-        # company_user_skills: params[:skills],
         job_category: params[:job_category]
     }.delete_if { |key, value| value.blank? }
     @listing_type_array = [params[:product],params[:service],params[:training]].reject { |listing_type| listing_type.blank? }
