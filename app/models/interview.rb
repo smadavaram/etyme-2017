da14cb25a1760a0e95cf9773ed980ec2ba864d59
Muhammad Umair Raza <umair.raza101@gmail.com>
2019-12-10 15:09:03 +0500

    fix interview issue in job application

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -2,7 +2,11 @@ class Interview < ApplicationRecord
   belongs_to :job_application
 
   def is_accepted?
-    accept and accepted_by_recruiter and accepted_by_company
+    if self.job_application.recruiter_company_id.blank?
+      accept  and accepted_by_company
+    else
+      accept and accepted_by_recruiter and accepted_by_company
+    end
   end
 
 end
