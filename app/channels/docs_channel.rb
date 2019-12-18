ccda2fdc3819546388a3456e5c07425e5cb93f47
Muhammad umair Raza <umair.raza101@gmail.com>
2019-12-18 19:01:52 +0500

    init the new cable channel for contract docs

---
+++ app/channels/docs_channel.rb
@@ -0,0 +1,16 @@
+class DocsChannel < ApplicationCable::Channel
+
+  def subscribed
+    stream_from "Doc_User_#{current_user.id}"
+  end
+
+  def unsubscribed
+  end
+
+  def send_notification(data)
+  end
+
+  def notify(data)
+  end
+
+end
