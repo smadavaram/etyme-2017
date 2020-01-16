4adecc6077b10171368b3103e9d107b0cce04151
Lalu Saud <lalusaud@gmail.com>
2020-01-16 16:39:41 -0500

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,13 +3,34 @@
 # Supports bulk-adding hosts to roles, the primary server in each group
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
- 
-role :app, %w{deploy@165.227.60.86}
-role :web, %w{deploy@165.227.60.86}
-role :db,  %w{deploy@165.227.60.86}
+
+# role :app, %w{deploy@165.227.60.86}
+# role :web, %w{deploy@165.227.60.86}
+# role :db,  %w{deploy@165.227.60.86}
+# set :deploy_to, '/var/www/deploy'
+# set :branch, 'deploy-dev'
+# set :rvm1_map_bins, %w{rake gem bundle ruby}
+#
+# # Extended Server Syntax
+# # ======================
+# # This can be used to drop a more detailed server definition into the
+# # server list. The second argument is a, or duck-types, Hash and is
+# # used to set extended properties on the server.
+#
+# server '165.227.60.86', user: 'deploy',  password: 'deploy', roles: %w{web app}
+
+
+# Simple Role Syntax
+# ==================
+# Supports bulk-adding hosts to roles, the primary server in each group
+# is considered to be the first unless any hosts have the primary
+# property set.  Don't declare `role :all`, it's a meta role.
+
+role :app, %w{deploy@157.230.90.106}
+role :web, %w{deploy@157.230.90.106}
+role :db,  %w{deploy@157.230.90.106}
 set :deploy_to, '/var/www/deploy'
 set :branch, 'deploy-dev'
-set :rvm1_map_bins, %w{rake gem bundle ruby}
 
 # Extended Server Syntax
 # ======================
@@ -17,4 +38,4 @@ set :rvm1_map_bins, %w{rake gem bundle ruby}
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server '165.227.60.86', user: 'deploy',  password: 'deploy', roles: %w{web app}
+server '157.230.90.106', user: 'deploy',  password: 'Etyme123@', roles: %w{web app}
