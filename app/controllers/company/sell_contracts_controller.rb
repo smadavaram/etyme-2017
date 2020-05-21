5ede8bec18955616eec6d5e5db6cb5725c8bbef1
Hammad Ahmed <hamad.ahmad@engin.tech>
2020-05-21 17:22:08 +0500

    Consultants issue fixed

--- app/controllers/company/sell_contracts_controller.rb
+++ app/controllers/company/sell_contracts_controller.rb
@@ -14,5 +14,8 @@ class Company::SellContractsController < Company::BaseController
     add_breadcrumb params[:id]
 
     @sell_contract = SellContract.where(number: params[:id]).first
+    @contract = @sell_contract.contract
+    @signature_documents = @contract.send('sell_contract').document_signs.where(documentable: @contract.company.company_candidate_docs.where(is_require: 'signature').ids)
+    @request_documents = @contract.send('sell_contract').document_signs.where(documentable: @contract.company.company_candidate_docs.where(is_require: 'Document').ids)
   end
 end
