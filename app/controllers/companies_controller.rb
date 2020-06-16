18db3b663a661fca3277864853b2fbcc88139129
Hammad Ahmed <hamad.ahmad@engin.tech>
2020-06-16 16:35:11 +0500

    small fix

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -51,7 +51,7 @@ class CompaniesController < ApplicationController
   private
 
   def redirect_to_main_domain
-    return redirect_to "#{HOSTNAME}/register" if request.subdomain.present?
+    return redirect_to "#{HOSTNAME}/register" if request.subdomain != 'app'
   end
 
   def find_company
