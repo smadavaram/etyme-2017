c94ed584061fdbe2a44aa4f6af05983fe90a7ac8
Hammad Ahmed <hamad.ahmad@engin.tech>
2020-06-25 15:29:06 +0500

    Allow access to manage company

--- app/controllers/company/base_controller.rb
+++ app/controllers/company/base_controller.rb
@@ -6,10 +6,12 @@ class Company::BaseController < ApplicationController
   layout 'company'
 
   def has_access?(permission)
+    return true if permission == "manage_company"
+
     if current_user.has_permission(permission) || current_user.is_owner?
       true
     else
-      flash[:notice] = 'You are not Authorized to Acess this Page'
+      flash[:notice] = 'You are not Authorized to Access this Page'
       redirect_to dashboard_path
     end
   end
