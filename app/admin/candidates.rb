531f1e13a3dbedb07f9e65ab8579ac2e0dda5fac
Tanay <tanays.tps@gmail.com>
2020-08-14 16:57:33 +0530

    add params

--- app/admin/candidates.rb
+++ app/admin/candidates.rb
@@ -21,10 +21,10 @@ ActiveAdmin.register Candidate do
   #
   # or
   #
-  # permit_params do
-  #   permitted = [:first_name, :last_name, :gender, :email, :phone, :time_zone, :primary_address_id, :photo, :signature, :status, :dob, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :sign_in_count, :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip, :last_sign_in_ip, :confirmation_token, :confirmed_at, :confirmation_sent_at, :unconfirmed_email, :invitation_token, :invitation_created_at, :invitation_sent_at, :invitation_accepted_at, :invitation_limit, :invited_by_type, :invited_by_id, :invitations_count, :resume, :skills, :description, :visa, :location, :video, :category, :subcategory, :dept_name, :industry_name, :video_type, :chat_status, :is_number_verify, :is_personal_info_update, :is_social_media, :is_education_detail_update, :is_skill_update, :is_client_info_update, :is_designate_update, :is_documents_submit, :is_profile_active, :selected_from_resume, :ever_worked_with_company, :designation_status, :candidate_visa, :candidate_title, :candidate_roal, :facebook_url, :twitter_url, :linkedin_url, :skypeid, :gtalk_url, :address, :company_id, :passport_number, :ssn, :relocation, :work_authorization, :visa_type, :latitude, :longitude, :skill_list, :designate_list]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
+  permit_params do
+    permitted = [:first_name, :last_name, :gender, :email, :phone, :time_zone, :primary_address_id, :photo, :signature, :status, :dob, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :sign_in_count, :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip, :last_sign_in_ip, :confirmation_token, :confirmed_at, :confirmation_sent_at, :unconfirmed_email, :invitation_token, :invitation_created_at, :invitation_sent_at, :invitation_accepted_at, :invitation_limit, :invited_by_type, :invited_by_id, :invitations_count, :resume, :skills, :description, :visa, :location, :video, :category, :subcategory, :dept_name, :industry_name, :video_type, :chat_status, :is_number_verify, :is_personal_info_update, :is_social_media, :is_education_detail_update, :is_skill_update, :is_client_info_update, :is_designate_update, :is_documents_submit, :is_profile_active, :selected_from_resume, :ever_worked_with_company, :designation_status, :candidate_visa, :candidate_title, :candidate_roal, :facebook_url, :twitter_url, :linkedin_url, :skypeid, :gtalk_url, :address, :company_id, :passport_number, :ssn, :relocation, :work_authorization, :visa_type, :latitude, :longitude, :skill_list, :designate_list]
+    permitted << :other if params[:action] == 'create' && current_user.admin?
+    permitted
+  end
 
 end
