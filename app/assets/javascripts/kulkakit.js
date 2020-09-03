f62ad47726f0de640bb21dd87835246f161f3f4b
parul <parulk.tps@gmail.com>
2020-09-03 18:04:05 +0530

    Added notification toast

--- app/assets/javascripts/kulkakit.js
+++ app/assets/javascripts/kulkakit.js
@@ -1,6 +1,13 @@
 //= require kulkakit/popper.min
 //= require kulkakit/jquery.min
+//= require static/js/jQuery-2.1.4.min
+//= require jquery-ui
 //= require jquery_ujs
+
 //= require kulkakit/bootstrap.min
+//= require plugin/bootstrap-timepicker/bootstrap-timepicker.min
+// = require_tree ./clean_admin/.
+//= require custom
 //= require trix
 //= require shared/uploader
+//= require notification/SmartNotification.min.js
