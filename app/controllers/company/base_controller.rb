0d4de3805d37f738541c7644a29152b73f3d86dd
Hammad Ahmed <hamad.ahmad@engin.tech>
2020-10-24 18:47:38 +0500

    Remove Permissions

--- app/controllers/company/base_controller.rb
+++ app/controllers/company/base_controller.rb
@@ -6,13 +6,14 @@ class Company::BaseController < ApplicationController
   layout 'company'
 
   def has_access?(permission)
-    return true if permission == "manage_company"
-    if current_user.has_permission(permission) || current_user.is_owner?
-      true
-    else
-      flash[:notice] = 'You are not Authorized to Acess this Page'
-      redirect_to dashboard_path
-    end
+    true
+    # return true if permission == "manage_company"
+    # if current_user.has_permission(permission) || current_user.is_owner?
+    #   true
+    # else
+    #   flash[:notice] = 'You are not Authorized to Acess this Page'
+    #   redirect_to dashboard_path
+    # end
   end
 
   helper_method :current_user
