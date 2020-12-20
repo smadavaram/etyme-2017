bb9f64e19f79abbdd31492be7a9281a58235f14e
Nitin Singh <nitin.singh@edcast.com>
2020-12-20 23:14:07 +0530

    Canges for yahoo app.

--- config/initializers/omnicontacts.rb
+++ config/initializers/omnicontacts.rb
@@ -2,9 +2,9 @@ require "omnicontacts"
 
 Rails.application.middleware.use OmniContacts::Builder do
   importer :gmail, ENV['GMAIL_CLIENT_ID'], ENV['GMAIL_CLIENT_SECRET'], {:redirect_path => "/contacts/gmail/import_contacts"}
-  # importer :yahoo, "consumer_id", "consumer_secret", {:callback_path => "/callback"}
-  # importer :linkedin, "consumer_id", "consumer_secret", {:redirect_path => "/oauth2callback", :state => '<long_unique_string_value>'}
-  # importer :hotmail, "client_id", "client_secret"
-  # importer :outlook, "app_id", "app_secret"
-  # importer :facebook, "client_id", "client_secret"
+  importer :yahoo, ENV['YAHOO_CLIENT_ID'], ENV['YAHOO_CLIENT_SECRET'], {:callback_path => "/contacts/yahoo/import_contacts"}
+  importer :linkedin, "consumer_id", "consumer_secret", {:redirect_path => "/oauth2callback", :state => '<long_unique_string_value>'}
+  importer :hotmail, "client_id", "client_secret"
+  importer :outlook, "app_id", "app_secret"
+  importer :facebook, "client_id", "client_secret"
 end
