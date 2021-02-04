57b1fd560a0f9aeec2f85e630191a08e164f7195
Iago Bozza <7201763-writeonlycode@users.noreply.gitlab.com>
2021-02-04 10:24:23 -0300

    Recert to original directories

--- app/controllers/company/directories_controller.rb
+++ app/controllers/company/directories_controller.rb
@@ -7,7 +7,6 @@ class Company::DirectoriesController < Company::BaseController
     add_breadcrumb 'My Directory'
 
     respond_to do |format|
-      raise
       format.html {}
       format.json { render json: CompanyDirectoryDatatable.new(params, view_context: view_context) }
     end
