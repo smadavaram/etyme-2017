b4bd6f2d6dd29937e5807a0115efab58d0b60e6f
Iago Bozza <7201763-writeonlycode@users.noreply.gitlab.com>
2021-02-10 13:06:51 -0300

    Update deployment information for staging

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -14,14 +14,14 @@
 # server '157.230.90.106', user: 'deploy',  password: 'Etyme123@', roles: %w{web app}
 #
 
-role :app, %w[deploy_user@3.128.51.36]
-role :web, %w[deploy_user@3.128.51.36]
-role :db,  %w[deploy_user@3.128.51.36]
+# role :app, %w[deploy_user@3.128.51.36]
+# role :web, %w[deploy_user@3.128.51.36]
+# role :db,  %w[deploy_user@3.128.51.36]
 set :deploy_to, '/var/www/etyme2020'
 set :repo_url, "git@github.com:smadavaram/etyme-2017.git"
 set :stage, :production
 
-# server "3.128.51.36", user: "deploy_user", roles: %w{app db web}
+server "3.128.51.36", user: "deploy_user", roles: %w{app db web}
 
 set :branch, 'deploy-staging'
 
