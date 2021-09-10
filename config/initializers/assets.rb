7b6d1c87e9f203d68bc5c8b1e55331b6d4c38a49
Hasan Afzal <hassan.afzal@techcreatix.com>
2021-09-10 18:26:21 +0500

    Working

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -16,6 +16,7 @@ Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w[company.js]
+Rails.application.config.assets.precompile += %w[cable.js]
 Rails.application.config.assets.precompile += %w[company.css]
 Rails.application.config.assets.precompile += %w[mycompany.css]
 
