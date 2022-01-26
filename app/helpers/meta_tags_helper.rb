40ab6f6644257a4458581c0649e1255f043027fc
ispirett <ispirett@gmail.com>
2022-01-26 01:16:44 -0400

    feature(tags)

---
+++ app/helpers/meta_tags_helper.rb
@@ -0,0 +1,19 @@
+module MetaTagsHelper
+  def meta_title
+    content_for?(:meta_title) ? content_for(:meta_title) : "Etyme"
+  end
+
+  def meta_description
+    content_for?(:meta_description) ? content_for(:meta_description) : "The Job matching agency"
+  end
+
+
+  def meta_url
+    content_for?(:meta_url) ? content_for(:meta_url) : request.original_url
+  end
+  def meta_image
+    meta_image = (content_for?(:meta_image) ? content_for(:meta_image) :"og.jpg")
+    #  helps make the image work with both assets and url
+    meta_image.starts_with?("http") ? meta_image : image_url(meta_image)
+  end
+end
