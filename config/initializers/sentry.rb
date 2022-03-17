0122308e912b6e98918458d761eb0b63eb42ed11
ispirett <ispirett@gmail.com>
2022-03-17 00:32:23 -0400

    chore(sentry):

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,5 @@
 Sentry.init do |config|
-  config.dsn = 'https://87650277c18a47b897ca3e64aca10ba8@o1134798.ingest.sentry.io/6183031'
+  config.dsn = 'https://acfa718e3a774ab3812c4df613f75ee3@o1169880.ingest.sentry.io/6263106'
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
   # Set tracesSampleRate to 1.0 to capture 100%
