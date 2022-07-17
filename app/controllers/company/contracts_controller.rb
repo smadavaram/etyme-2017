7714dcba30d0633fc79ac34cdb30d3d7aecffda5
Krrish Jha <kisanjha@gmail.com>
2022-07-18 02:31:12 +0530

    skip background job for contact

--- app/controllers/company/contracts_controller.rb
+++ app/controllers/company/contracts_controller.rb
@@ -408,7 +408,7 @@ class Company::ContractsController < Company::BaseController
 
   def generate_cycles
     if @contract.remaining?
-      GenerateContractCyclesJob.perform_later(@contract)
+      GenerateContractCyclesJob.perform_now(@contract)
       flash[:success] = 'Your request has been queued, will be processed momentarily'
       # else
       #   flash[:errors] = @contract.errors.full_messages << 'Errors while processing request'
