79ac2144416391947ea871f83e1b33c61304bca3
Asim Sikka <asim@amrood.de>
2022-09-22 00:43:06 +0500

    allow other domains to make cookies

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,4 +2,4 @@
 
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_etyme_session', domain: ENV["domain"] || ENV["DOMAIN"]
+Rails.application.config.session_store :cookie_store, key: '_etyme_session', domain: :all
