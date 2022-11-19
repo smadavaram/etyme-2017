55f3039bc050ba09d2200bed6629b0bebfff93e4
zain-asif-qbatch <zain.asif@qbatch.com>
2022-11-20 03:01:51 +0500

    disabled delayed jobs and added sidekiq

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: bin/rails s
-worker: bundle exec sidekiq -C config/sidekiq.yml
+worker: bundle exec sidekiq -e production -C config/sidekiq.yml
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,10 @@
+# frozen_string_literal: true
+
+# inside config/initializers/sidekiq.rb
+
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1') }
+  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://127.0.0.1:6379/1') }
 end
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1') }
+  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://127.0.0.1:6379/1') }
 end
