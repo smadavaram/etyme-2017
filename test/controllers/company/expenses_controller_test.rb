0745645fde7e1009c24aea142264fab82965c868
paul.chesterton <paul.chesterton@adlumin.com>
2023-06-21 16:58:34 -0400

    remove test files and start fresh

--- test/controllers/company/expenses_controller_test.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'test_helper'
-
-class Company::ExpensesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/mailers/candidate/candidate_mailer_test.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'test_helper'
-
-class Candidate::CandidateMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
