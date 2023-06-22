4173fda7f16b6dad7db83666008e8fb25f71e9c2
paul.chesterton <paul.chesterton@adlumin.com>
2023-06-22 12:01:31 -0400

    add debugging code

--- test/system/candidates_test.rb
+++ test/system/candidates_test.rb
@@ -2,6 +2,7 @@ require "application_system_test_case"
 
 class CandidatesTest < ApplicationSystemTestCase
   setup do
+    puts 'TOP OF SETUP BLOCK'
     candidate = Candidate.new(
       first_name: 'Cassian',
       last_name: 'Andor',
@@ -11,20 +12,34 @@ class CandidatesTest < ApplicationSystemTestCase
     )
     candidate.save
     candidate.confirm
+    puts "CANDIDATE SAVED: #{candidate.inspect}"
   end
 
   test "logging in a candidate" do
+    puts 'TOP OF TEST BLOCK'
     visit root_url
+    puts 'AFTER VISIT ROOT_URL'
     assert_selector "button", text: "Login"
+    puts 'AFTER ASSERT SELECTOR BUTTON'
     assert_no_selector "a", text: "Candidate"
+    puts 'AFTER ASSERT NO SELECTOR A'
     click_on "Login"
+    puts 'AFTER CLICK ON LOGIN'
     assert_selector "a", text: "Candidate"
+    puts 'AFTER ASSERT SELECTOR A'
     click_on "Candidate"
+    puts 'AFTER CLICK ON CANDIDATE'
     sleep 0.5
+    puts 'AFTER SLEEP'
     assert_selector "h4", text: "Sign In"
+    puts 'AFTER ASSERT SELECTOR H4'
     fill_in "candidate_email", with: "cassian@andor.com"
+    puts 'AFTER FILL IN EMAIL'
     fill_in "candidate_password", with: 'cassianandor'
+    puts 'AFTER FILL IN PASSWORD'
     find('input[type="submit"]').click
+    puts 'AFTER FIND INPUT TYPE SUBMIT CLICK'
     assert_no_selector "button", text: "Login"
+    puts 'AFTER ASSERT NO SELECTOR BUTTON'
   end
 end
