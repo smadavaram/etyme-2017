091ad6685a76660e7fbd95198313ec07311cb1ce
paul.chesterton <paul.chesterton@adlumin.com>
2023-06-23 15:07:12 -0400

    remove debugging code

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,8 +1,9 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  # driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
-  driven_by :selenium, using: :chrome, options: { args: ["headless", "disable-gpu", "no-sandbox", "disable-dev-shm-usage"] }
+  driven_by :selenium, using: :chrome, options: { args: %w[headless disable-gpu no-sandbox disable-dev-shm-usage] }
 
   Capybara.register_driver :selenium do |app|
     client = Selenium::WebDriver::Remote::Http::Default.new
