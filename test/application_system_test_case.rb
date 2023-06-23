081aea9ebd27c9b94721996083235ed89fe46020
paul.chesterton <paul.chesterton@adlumin.com>
2023-06-23 10:22:12 -0400

    troubleshooting GitHub action

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,6 +1,7 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+  # driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+  driven_by :selenium, using: :chrome, options: { args: ["headless", "disable-gpu", "no-sandbox", "disable-dev-shm-usage"] }
   Capybara.default_max_wait_time = 10 # seconds
 end
