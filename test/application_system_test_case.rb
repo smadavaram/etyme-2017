69ddde4997a6e6232e301c13ae9231ea6b634292
paul.chesterton <paul.chesterton@adlumin.com>
2023-06-23 10:47:40 -0400

    continue debugging GitHub action

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,10 +3,13 @@ require "test_helper"
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   # driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
   driven_by :selenium, using: :chrome, options: { args: ["headless", "disable-gpu", "no-sandbox", "disable-dev-shm-usage"] }
-  Capybara.default_max_wait_time = 10 # seconds
+
   Capybara.register_driver :selenium do |app|
     client = Selenium::WebDriver::Remote::Http::Default.new
     client.read_timeout = 120 # seconds
     Capybara::Selenium::Driver.new(app, browser: :chrome, http_client: client)
   end
+  Capybara.server_boot_timeout = 120 # seconds
+  Capybara.default_max_wait_time = 10 # seconds
+  Capybara.server = :webrick
 end
