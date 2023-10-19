19a50019a03b52b084e9f3b62907c90e3341e1f3
sagar535 <dsl.sagar535@gmail.com>
2023-10-19 09:17:05 +0545

    send exception to me change address to host

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,7 +6,7 @@ class UserMailer < ApplicationMailer
     @name = name
     @exception = exception
     # mail(to: UserMailer.exception_admins, subject: 'Etyme - Exception')
-    mail(to: %w[mirzamoiz65@gmail.com], subject: 'Etyme - Exception')
+    mail(to: %w[dit.sagar@gmail.com], subject: 'Etyme - Exception')
   end
 
   def confirmation_instructions(user, token, _opts = {})
